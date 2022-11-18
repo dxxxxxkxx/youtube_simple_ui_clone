0676f1c280738d1fed02048e6a954d172641f1c7
Dohyun Kim <dxxxxxkxx@gmail.com>
2022-11-18 16:28:48 +0900

    temp: 비디오 밑에 아이콘 삭제

--- lib/components/video_play_component.dart
+++ lib/components/video_play_component.dart
@@ -23,11 +23,8 @@ class _VideoPlayComponentState extends State<VideoPlayComponent>
   late PlayerState _playerState;
   late YoutubeMetaData _videoMetaData;
 
-  double _volume = 100;
-  bool _isMuted = false;
+  double _volume = 50;
   bool _isPlayerReady = false;
-
-  final List<String?> _ids = [];
 
   @override
   void initState() {
@@ -35,7 +32,6 @@ class _VideoPlayComponentState extends State<VideoPlayComponent>
 
     _controller = YoutubePlayerController(
       initialVideoId: YoutubePlayer.convertUrlToId(widget.videoSrc)!,
-      //_ids.first,
       flags: const YoutubePlayerFlags(
         mute: false,
         autoPlay: true,
@@ -54,7 +50,7 @@ class _VideoPlayComponentState extends State<VideoPlayComponent>
   }
 
   void listener() {
-    if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+    if (mounted && _isPlayerReady && !_controller.value.isFullScreen) {
       setState(() {
         _playerState = _controller.value.playerState;
         _videoMetaData = _controller.metadata;
@@ -87,7 +83,7 @@ class _VideoPlayComponentState extends State<VideoPlayComponent>
       player: YoutubePlayer(
         controller: _controller,
         showVideoProgressIndicator: true,
-        progressIndicatorColor: Colors.blueAccent,
+        progressIndicatorColor: Colors.green,
         topActions: [
           const SizedBox(width: 8.0),
           Expanded(
@@ -113,102 +109,38 @@ class _VideoPlayComponentState extends State<VideoPlayComponent>
           player,
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        _controller.value.isPlaying
-                            ? Icons.pause
-                            : Icons.play_arrow,
-                        color: Colors.white,
-                      ),
-                      onPressed: _isPlayerReady
-                          ? () {
-                              _controller.value.isPlaying
-                                  ? _controller.pause()
-                                  : _controller.play();
-                              setState(() {});
-                            }
-                          : null,
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        _isMuted ? Icons.volume_off : Icons.volume_up,
-                        color: Colors.white,
-                      ),
-                      onPressed: _isPlayerReady
-                          ? () {
-                              _isMuted
-                                  ? _controller.unMute()
-                                  : _controller.mute();
-                              setState(() {
-                                _isMuted = !_isMuted;
-                              });
-                            }
-                          : null,
-                    ),
-                    FullScreenButton(
-                      controller: _controller,
-                      color: Colors.blueAccent,
-                    ),
-                  ],
+            child: Row(
+              children: <Widget>[
+                const Text(
+                  'Volume',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white,
+                  ),
                 ),
-                _space,
-                Row(
-                  children: <Widget>[
-                    const Text(
-                      "Volume",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300, color: Colors.white),
-                    ),
-                    Expanded(
-                      child: Slider(
-                        inactiveColor: Colors.transparent,
-                        value: _volume,
-                        min: 0.0,
-                        max: 100.0,
-                        divisions: 10,
-                        label: '${(_volume).round()}',
-                        onChanged: _isPlayerReady
-                            ? (value) {
-                                setState(() {
-                                  _volume = value;
-                                });
-                                _controller.setVolume(_volume.round());
-                              }
-                            : null,
-                      ),
-                    )
-                  ],
+                Expanded(
+                  child: Slider(
+                    inactiveColor: Colors.transparent,
+                    value: _volume,
+                    min: 0.0,
+                    max: 100.0,
+                    divisions: 10,
+                    label: '${(_volume).round()}',
+                    onChanged: _isPlayerReady
+                        ? (value) {
+                            setState(() {
+                              _volume = value;
+                            });
+
+                            _controller.setVolume(_volume.round());
+                          }
+                        : null,
+                  ),
                 )
               ],
             ),
           )
         ],
-      ),
-    );
-  }
-
-  Widget get _space => const SizedBox(height: 10);
-
-  void _showSnackBar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          message,
-          textAlign: TextAlign.center,
-          style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 16.0),
-        ),
-        backgroundColor: Colors.blueAccent,
-        behavior: SnackBarBehavior.floating,
-        elevation: 1.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50.0),
-        ),
       ),
     );
   }
